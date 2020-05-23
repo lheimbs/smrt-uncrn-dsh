3b069f0d94dcfe3e0b65fb3b2b1afed9422080b4
Lennart Heimbs <l.heimbs@gmail.com>
2020-05-23 23:46:31 +0200

    Remove live mqtt section

--- dashboard/mqtt/mqtt.py
+++ dashboard/mqtt/mqtt.py
@@ -2,54 +2,101 @@
 
 import logging
 
+import dash_table
 import dash_core_components as dcc
 import dash_html_components as html
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 
-from app import app
-import mqtt.messages
-import mqtt.live
+from app import app, COLORS
+from mqtt.sql import (
+    get_mqtt_topics_as_options,
+    get_mqtt_messages_by_topic,
+)
 
 logger = logging.getLogger()
 
 layout = html.Div([
-    dcc.Tabs(
-        id="mqtt-main-tabs",
-        persistence=True,
-        # value="mqtt-messages-tab",
-        parent_className='custom__main__tabs',
-        className='custom__main__tabs__container',
-        children=[
-            dcc.Tab(
-                label="Messages",
-                value="mqtt-messages-tab",
-                className='custom__main__sub__tab',
-                selected_className='custom__main__sub__tab____selected',
-            ),
-            dcc.Tab(
-                label="Live",
-                value="mqtt-live-tab",
-                className='custom__main__sub__tab',
-                selected_className='custom__main__sub__tab____selected',
-            ),
-        ]
-    ),
     html.Div(
-        id="mqtt-tabs-content",
-        className="app__tab__content"
+        className='row',
+        children=[
+            html.Div(
+                className='two columns settings',
+                children=[
+                    html.H6("Select topics:"),
+                    dcc.Loading(id="loading-mqtt-topics", color=COLORS['foreground'], children=[
+                        dcc.Checklist(
+                            id="mqtt-select-topics",
+                            value=[],
+                            labelStyle={'display': 'block'},
+                            className="mqtt__topic__select",
+                        ),
+                    ]),
+                    html.H6("Select number of entries:"),
+                    dcc.Input(
+                        id='mqtt-select-num-msgs',
+                        type='number',
+                        value=1000,
+                        min=1,
+                        max=99999,
+                        debounce=True,
+                    )
+                ],
+            ),
+            html.Div(
+                className='ten columns',
+                children=[
+                    dcc.Loading(id="loading-mqtt-messages", type="default", color=COLORS['foreground'], children=[
+                        dash_table.DataTable(
+                            id='table-mqtt-messages',
+                            columns=[
+                                {"name": 'Date', "id": 'date'},
+                                {"name": 'Time', "id": 'time'},
+                                {"name": 'Topic', "id": 'topic'},
+                                {"name": 'Payload', "id": 'payload'},
+                            ],
+                            data=[],  # data.to_dict('records'),
+
+                            page_action="native",
+                            page_current=0,
+                            page_size=25,
+                            style_as_list_view=True,
+                            style_header={
+                                'backgroundColor': COLORS['background-medium'],
+                                'fontWeight': 'bold'
+                            },
+                            style_cell={
+                                'padding': '5px',
+                                'textAlign': 'center',
+                                'backgroundColor': COLORS['background'],
+                            },
+                        ),
+                    ]),
+                ],
+            )
+        ],
     ),
 ])
 
 
-@app.callback(Output('mqtt-tabs-content', 'children'),
-              [Input('mqtt-main-tabs', 'value')])
-def render_mqtt_content(tab):
-    if tab == 'mqtt-messages-tab':
-        layout = mqtt.messages.layout
-    elif tab == 'mqtt-live-tab':
-        layout = mqtt.live.layout
-    elif tab == 'mqtt-settings-tab':
-        layout = html.Div("Settings")
+@app.callback(
+    Output('mqtt-select-topics', 'options'),
+    [Input('mqtt-select-topics', 'loading_state')],
+    [State('error-store', 'data')]
+)
+def mqtt_topics_as_options(_, errors):
+    if errors['mqtt-message']:
+        logger.warning("Mqtt-Messages table does not exist. Cant fetch mqtt topics.")
+        return []
+    return get_mqtt_topics_as_options()
+
+
+@app.callback(Output('table-mqtt-messages', 'data'),
+              [Input('mqtt-select-topics', 'value'),
+               Input('mqtt-select-num-msgs', 'value')])
+def get_table_data(selected_topics, limit):
+    if selected_topics and limit:
+        logger.debug(f"MQTT Messages callback. Topics: {selected_topics}, limit: {limit}.")
+        data = get_mqtt_messages_by_topic(selected_topics, limit)
+        return data.to_dict('records')
     else:
-        layout = mqtt.messages.layout
-    return layout
+        return []
--- dashboard/mqtt/sql.py
+++ dashboard/mqtt/sql.py
@@ -1,3 +1,26 @@
 #!/usr/bin/env python3
 
+import logging
+import pandas as pd
 
+from app import db
+from models.Mqtt import Mqtt
+
+logger = logging.getLogger()
+
+def get_mqtt_topics_as_options():
+    topics = []
+    for topic in db.session.query(Mqtt.topic).distinct().order_by(Mqtt.topic):
+        topics.append({'label': topic.topic, 'value': topic.topic})
+    return topics
+
+
+def get_mqtt_messages_by_topic(topics, limit):
+    messages_dict = []
+    messages = Mqtt.query.filter(Mqtt.topic.in_(topics)).order_by(Mqtt.date).limit(limit)
+    for message in messages:
+        message = message.to_dict()
+        message.update({'time': message['date'].time()})
+        message['date'] = message['date'].date()
+        messages_dict.append(message)
+    return pd.DataFrame(messages_dict, columns=['date', 'time', 'topic', 'payload', 'retain', 'qos'])
