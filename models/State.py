d5398ec738523e5ba4e01234f637c5d16583316c
Lennart Heimbs <l.heimbs@gmail.com>
2020-05-29 12:48:38 +0200

    Move models to top-level directory

---
+++ models/State.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python3
+
+from dashboard.app import db
+
+class State(db.Model):
+    __tablename__ = 'states'
+
+    id = db.Column(db.Integer, primary_key=True)
+    date = db.Column(db.DateTime, nullable=False)
+    device = db.Column(db.String)
+    state = db.Column(db.String)
+
+    def to_dict(self):
+        return {
+            'date': self.date,
+            'device': self.device,
+            'state': self.state,
+        }
+
+    def __repr__(self):
+        return (
+            "<State(id={self.id}, "
+            f"date={self.date}, "
+            f"device={self.device}, "
+            f"state={self.state})>"
+        )
