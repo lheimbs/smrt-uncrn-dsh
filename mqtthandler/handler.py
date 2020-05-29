f64f0169b1268edee7bd07534e323bb716dd33a1
Lennart Heimbs <l.heimbs@gmail.com>
2020-05-29 12:37:01 +0200

    Add developbent environment for mqtthandler
    - Starts a local mqtt broker (requiring mosquitto mqtt)
    - Generates random data

--- mqtthandler/handler.py
+++ mqtthandler/handler.py
@@ -4,11 +4,15 @@
 import time
 import logging
 import socket
-import json
+import subprocess
+import random
 import paho.mqtt.client as mqtt
 from datetime import datetime
 
+from mqtthandler.PipeLogging import LogPipe
+from mqtthandler.random_data import random_publish
 import mqtthandler.sql as sql
+
 from mqtthandler.callbacks import (
     temp_message_to_db,
     handle_rf_transmission,
@@ -18,16 +22,19 @@ from mqtthandler.callbacks import (
     handle_tablet_charging
 )
 
+import sys
+sys.path.insert(0, '..')
+from dashboard.app import server            # noqa E402
+
 logger = logging.getLogger('mqtt_handler')
-logger.debug('mqtthandler debug')
-logger.info('mqtthandler info')
-logger.warning('mqtthandler warning')
-logger.error('mqtthandler error')
 
 
-def connect(client):
-    brokers = ['localhost', 'lennyspi.local', '192.168.1.201', '192.168.1.205']
-    port = 8883
+def connect(client, brokers=None, port=None):
+    if not brokers:
+        brokers = ['localhost', 'lennyspi.local', '192.168.1.201', '192.168.1.205']
+    if not port:
+        port = 8883
+
     for host in brokers:
         sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
         logger.debug(f"Try connection to broker {host} on port {port}.")
@@ -68,6 +75,7 @@ def on_message(client, userdata, msg):
 
 def main():
     client = mqtt.Client()
+    broker = None
 
     mqtt_logger = logging.getLogger('client')
     mqtt_logger.setLevel(logging.INFO)
@@ -86,13 +94,50 @@ def main():
     client.message_callback_add("mqtt/voice_assistant/status", handle_states)
 
     # connect to broker
-    client = connect(client)
+    if server.config['DEBUG']:
+        logger.info(f"Using development broker: {server.config['MQTT_SERVER']}.")
+
+        sys.stdout = LogPipe(logging.DEBUG, 'local_broker')
+        sys.stderr = LogPipe(logging.INFO, 'local_broker')
+
+        broker = subprocess.Popen(
+            ['mosquitto', '-p', str(server.config['MQTT_PORT'])],
+            stdout=sys.stdout,
+            stderr=sys.stderr
+        )
+        logger.debug(f"Starting development broker locally. PID: {broker.pid}.")
+        time.sleep(1)
+
+        client = connect(client, (server.config['MQTT_SERVER'],), server.config['MQTT_PORT'])
+    else:
+        client = connect(client)
 
     try:
+        if server.config['DEBUG']:
+            rand_time = random.randint(3, 9)
+            choice = 'roomdata'
+            client.loop_start()
+
         while client:
-            client.loop()
+            if server.config['DEBUG']:
+                if time.time() % rand_time == 0:
+                    choice = random_publish(choice, server.config['MQTT_PORT'])
+                    rand_time = random.randint(10, 90)
+            else:
+                client.loop()
+
     except KeyboardInterrupt:
         logger.info("Script stopped through Keyboard Interrupt")
+        if server.config['DEBUG']:
+            client.loop_stop()
     finally:
         logger.info("Disconnecting client from broker.")
-        client.disconnect()
+        if client and client.is_connected:
+            client.disconnect()
+        if broker:
+            # close the file handlers properly
+            sys.stdout.close()
+            sys.stdout = sys.__stdout__
+            sys.stderr.close()
+            sys.stderr = sys.__stderr__
+            broker.terminate()
