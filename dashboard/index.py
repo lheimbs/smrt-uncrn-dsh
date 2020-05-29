973c64d149a18568357105a3ae907410fc255397
Lennart Heimbs <l.heimbs@gmail.com>
2020-05-29 12:31:36 +0200

    Add method to generate debugging shoping data and use database connection instead of path for error detection

--- dashboard/index.py
+++ dashboard/index.py
@@ -1,38 +1,70 @@
 #!/usr/bin/env python3
 
-import os
 import logging
+from datetime import datetime
 
 import dash_core_components as dcc
 import dash_html_components as html
 from dash.dependencies import Input, Output
 
 # page import here, to get the supplied log level in modules (# noqa E420?)
-from .app import app, db, DATABASE_PATH, DATABASE_PROBES_PATH
+from .app import app, db, server
 from . import shopping
 from . import general
 from . import data
 from . import mqtt
-# from .shopping 
+from models.Shopping import List, Shop, Category, Item
 
 logger = logging.getLogger()
 
 
+def generate_shopping_data():
+    cat = Category(name='Lebensmittel')
+    shop = Shop(name='REWE', category=cat)
+    items = [
+        Item(name='Bier', price=1.09, volume='', price_per_volume='', sale=False, note=''),
+        Item(name='Pfand', price=0.08, volume='', price_per_volume='', sale=False, note=''),
+        Item(name='Leergut', price=0.08, volume='', price_per_volume='', sale=False, note=''),
+        Item(name='Kinder Bueno', price=1.99, volume='', price_per_volume='', sale=True, note=''),
+        Item(name='Schokobons', price=1.99, volume='200g', price_per_volume='', sale=False, note=''),
+        Item(name='Eis Stiel-Brownie', price=1.69, volume='', price_per_volume='', sale=False, note=''),
+    ]
+    list_1 = List(date=datetime(2020, 5, 16, 0, 0), price=6.76, shop=shop, items=items)
+
+    list_2 = List(date=datetime(2019, 5, 15, 0, 0), price=3.61, shop=Shop(name='EDEKA', category=cat), items=[
+        Item(name='Toast', price=0.59, volume='', price_per_volume='', sale=False, note=''),
+        Item(name='Schokobons klein', price=1.99, volume='', price_per_volume='', sale=False, note=''),
+        Item(name='Joghurt', price=0.88, volume='', price_per_volume='', sale=False, note='0,15'),
+        Item(name='Pfand', price=0.15, volume='', price_per_volume='', sale=False, note='')
+    ])
+
+    db.session.add(list_1)
+    db.session.add(list_2)
+    db.session.commit()
+
+
 def precheck_errors():
-    tables = ['room-data', 'rf-data', 'mqtt-message', 'probe-request', 'list', 'item', 'shop', 'category']
+    tables = ['room-data', 'rf-data', 'mqtt-message', 'list', 'item', 'shop', 'category']
     errors_str = []
     errors_dict = {
         'database': False,
         'database-probes': False,
     }
-    if not os.path.exists(DATABASE_PATH):
-        logger.warning(f"Database not found under '{DATABASE_PATH}.")
-        errors_str.append(f"ERROR: Database not found under '{DATABASE_PATH}.")
+    try:
+        db.session.execute("SELECT 1")
+    except Exception as exc:
+        logger.warning("Database not found.")
+        logger.debug(exc)
+        errors_str.append("ERROR: Database not found.")
         errors_dict['database'] = True
-    if not os.path.exists(DATABASE_PROBES_PATH):
-        logger.warning(f"Database not found under '{DATABASE_PROBES_PATH}.")
-        errors_str.append(f"ERROR: Database for Probe Requests not found under '{DATABASE_PROBES_PATH}.")
+    try:
+        db.session.execute("SELECT 1", bind=db.get_engine(server, 'probe-requests'))
+    except Exception as exc:
+        logger.warning("Database for Probe-Requests not found.")
+        logger.debug(exc)
+        errors_str.append("ERROR: Database for Probe-Requests not found.")
         errors_dict['database-probes'] = True
+
     for table in tables:
         if db.engine.has_table(table):
             errors_dict[table] = False
@@ -85,7 +117,6 @@ layout = html.Div(
                         ),
                     ]
                 ),
-
                 html.Div(
                     id="main-tabs-content",
                     className="content"
@@ -128,6 +159,13 @@ def update_error_store(_):
 
 
 if __name__ == "__main__":
+    logger.info(f"Environment: {server.config['ENV']}")
+    logger.info(f"Debug: {server.config['DEBUG']}")
+    logger.info(f"Secret key: {server.config['SECRET_KEY']}")
+
+    if server.config['DEBUG']:
+        generate_shopping_data()
+
     app.layout = layout
     app.run_server(debug=True, port=5002, host='0.0.0.0', threaded=True)
     # Deleted 'self.logger.setLevel' from dash.py so debug messages are getting logged in callbacks
