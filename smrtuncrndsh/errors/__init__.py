bc1d50cc0a9c2e0d4b758ce9bc56dd45e4e98254
Lennart Heimbs <l.heimbs@gmail.com>
2020-07-23 12:53:46 +0200

    Add crumbling error message

--- smrtuncrndsh/errors/__init__.py
+++ smrtuncrndsh/errors/__init__.py
@@ -2,26 +2,38 @@ from flask import render_template
 
 
 def register_handlers(app):
-    if app.config.get('DEBUG') is True:
-        app.logger.debug('Skipping error handlers in Debug mode')
-        return
+    # if app.config.get('DEBUG') is True:
+    #     app.logger.debug('Skipping error handlers in Debug mode')
+    #     return
 
     @app.errorhandler(403)
     def forbidden_page(*args, **kwargs):
         # do stuff
-        return render_template("errors/403.html"), 403
+        return render_template(
+            "errors/error.html", title="Error 403", error_code="403",
+            error_msg="Bummer, it seems like your don't have access to this page :("
+        ), 403
 
     @app.errorhandler(404)
     def page_not_found(*args, **kwargs):
         # do stuff
-        return render_template("errors/404.html"), 404
+        return render_template(
+            "errors/error.html", title="Error 404", error_code="404",
+            error_msg="Damn, this page does not seem to exist :/"
+        ), 404
 
     @app.errorhandler(410)
     def page_gone(*args, **kwargs):
         # do stuff
-        return render_template("errors/405.html"), 410
+        return render_template(
+            "errors/error.html", title="Error 410", error_code="410",
+            error_msg="Oh man, this page is gone for good... Maybe try another one?"
+        ), 410
 
     @app.errorhandler(500)
     def server_error_page(*args, **kwargs):
         # do stuff
-        return render_template("errors/500.html"), 500
+        return render_template(
+            "errors/error.html", title="Error 500", error_code="500",
+            error_msg="Yikes, this one is on me. I must have fucked up the programming or something >.<"
+        ), 500
