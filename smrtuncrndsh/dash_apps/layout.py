77e83fdd1b51a1c9acf7cd3da5e87e7c5a12d17f
Lennart Heimbs <l.heimbs@gmail.com>
2020-07-28 23:49:00 +0200

    move dashapp to use template without string edits remove scripts hash since no scripts are used there

--- smrtuncrndsh/dash_apps/layout.py
+++ smrtuncrndsh/dash_apps/layout.py
@@ -1,21 +1,16 @@
 """Plotly Dash HTML layout override."""
 
-import hashlib
-import base64
-from typing import List
-
 from dash import Dash
 from flask import render_template, current_app
+
 from flask_login import login_required
 
-from smrtuncrndsh import csrf, talisman
+from smrtuncrndsh import csrf
 from smrtuncrndsh.auth import activation_required
 
 
 def register_dash_app(app, dash_app, *args):
     dash_app.init_app(app)
-
-    current_app.config['CSP']['script-src'] += calculate_inline_hashes(dash_app)
 
     if app.config['DEBUG']:
         dash_app.enable_dev_tools(debug=True)
@@ -25,40 +20,26 @@ def register_dash_app(app, dash_app, *args):
     for view_name, view_method in dash_app.server.view_functions.items():
         if view_name.startswith(dash_app.config['routes_pathname_prefix']):
             csrf.exempt(view_method)
-            if 'activation_required' in args:
+            if 'activation_required' in args and not current_app.config['DEBUG']:
                 dash_app.server.view_functions[view_name] = activation_required(view_method)
-            elif 'login_required' in args:
+            elif 'login_required' in args and not current_app.config['DEBUG']:
                 dash_app.server.view_functions[view_name] = login_required(view_method)
 
 
-class DashFlaskyfied(Dash):
-    def __init__(self, template_str='', title='', *args, **kwargs):
+class CustomDash(Dash):
+    def __init__(self, template='', title='', *args, **kwargs):
         Dash.__init__(self, *args, **kwargs)
-        self.template_str = template_str
+        self.template_str = template
         self.title = title
 
     def interpolate_index(self, **kwargs):
         current_app.logger.debug("serve index")
         template = render_template('dash_base.html', template=self.template_str, title=self.title)
-        idx = template.index('<main>') + len('<main>')
-        template = template[:idx] + f'{kwargs["app_entry"]}' + template[idx:]
-        idx = template.index('</body>')
-        template = template[:idx] + (
-            f'<dash_footer>{kwargs["config"]}{kwargs["scripts"]}{kwargs["renderer"]}</dash_footer>'
-        ) + template[idx:]
+        template = template.format(
+            css=kwargs['css'],
+            app_entry=kwargs['app_entry'],
+            config=kwargs['config'],
+            scripts=kwargs['scripts'],
+            renderer=kwargs['renderer']
+        )
         return template
-
-
-def calculate_inline_hashes(app: Dash) -> List[str]:
-    """Given a Dash app instance, this function calculates
-    CSP hashes (sha256 + base64) of all inline scripts, such that
-    one of the biggest benefits of CSP (banning general inline scripts)
-    can be utilized.
-
-    Warning: Note that this function uses a "private" Dash app attribute,
-    app._inline_scripts, which might change from one Dash version to the next.
-    """
-    return [
-        f"'sha256-{base64.b64encode(hashlib.sha256(script.encode('utf-8')).digest()).decode('utf-8')}'"
-        for script in [app.renderer] + app._inline_scripts
-    ]
