6187c5bfbe4ffe676f5b00965ef49671d21e999b
Lennart Heimbs <l.heimbs@gmail.com>
2020-12-16 23:58:06 +0100

    Add optional class argument

--- smrtuncrndsh/static/js/flash.js
+++ smrtuncrndsh/static/js/flash.js
@@ -1,10 +1,10 @@
 
-function makeFlashMessage(type, message) {
+function makeFlashMessage(type, message, flashclass = "flashes") {
     new_message = $("<div>").addClass("flash message "+type)
         .append($("<span>").text(message))
         .append($("<i>").addClass("close-flash material-icons").text("close"));
 
-    $(new_message).appendTo(".flashes");
+    $(new_message).appendTo("."+flashclass);
 }
 
 function makeAddItemFlashMessage(type, message) {
