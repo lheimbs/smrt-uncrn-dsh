b8351770b30a1169e360f430167c792bbc5e2ed8
Lennart Heimbs <l.heimbs@gmail.com>
2020-12-16 23:51:11 +0100

    Add forms for receipt scanner

--- smrtuncrndsh/shopping_add/forms.py
+++ smrtuncrndsh/shopping_add/forms.py
@@ -1,8 +1,8 @@
 from flask_wtf import FlaskForm
 from wtforms.fields.html5 import DateField, DecimalField, IntegerField
-from wtforms import TextField, FieldList, FormField, BooleanField
-from wtforms.validators import DataRequired
-from flask_wtf.file import FileField, FileAllowed, FileRequired
+from wtforms import TextField, FieldList, FormField, BooleanField, RadioField
+from wtforms.validators import DataRequired, Optional
+from flask_wtf.file import FileField, FileRequired
 
 
 class AddItem(FlaskForm):
@@ -35,28 +35,35 @@ class AddList(FlaskForm):
 
 class PdfForm(FlaskForm):
     reciept = FileField(validators=[FileRequired()])
-    shop = TextField("Shop", validators=[])
+    # submit = SubmitField('Submit')
 
 
 class ReceiptItem(FlaskForm):
-    name = TextField("Name", validators=[DataRequired()])
-    price_per_piece = DecimalField("Price per piece", validators=[DataRequired()])
+    item = TextField("Name")
+    price_per_piece = DecimalField("Price per piece")
     total_price = DecimalField("Total Price")
-    amount = IntegerField("Amount", validators=[DataRequired()])
+    amount = IntegerField("Amount")
     volume = TextField("Volume")
     ppv = TextField("Price per Volume")
+    sale = BooleanField("Sale")
+
+
+class ShopSelectForm(FlaskForm):
+    # select = BooleanField("Select")
+    shop = TextField("Shop", validators=[DataRequired()])
+    category = TextField("Category")
 
 
 class ReceiptForm(FlaskForm):
     date = DateField("Date", validators=[DataRequired()])
-    price = DecimalField("Price", validators=[DataRequired()])
+    sums = RadioField("Prices", coerce=float, validate_choice=False)
+    price = DecimalField("Price", validators=[Optional()])
+    category = TextField("Category")
     shops = FieldList(
-        TextField("Shop"),
+        FormField(ShopSelectForm),
         min_entries=1,
     )
-    category = TextField("Category")
-
-    new_items = FieldList(
-        FormField(ReceiptItem, label=None),
-        min_entries=0,
+    items = FieldList(
+        FormField(ReceiptItem),
+        min_entries=1,
     )
