c6c7015f7ff377e7e2b826f035ea7ec02b93f144
Lennart Heimbs <l.heimbs@gmail.com>
2020-12-20 12:44:18 +0100

    add file skipping of lib files

--- smrtuncrndsh/clean_for_prod.py
+++ smrtuncrndsh/clean_for_prod.py
@@ -5,22 +5,24 @@ def clean_for_prod():
     BASE_DIR = os.path.abspath(os.path.abspath(os.path.dirname(__file__)))
     print(BASE_DIR)
     for root, dirs, files in os.walk(BASE_DIR):
-        path = root.split(os.sep)
         for file in files:
             file_path = os.path.join(root, file)
 
             if file.endswith(".py") or file.endswith(".html") or file.endswith(".js"):
-                print(file_path)
-                if 
-
+                if file_path.startswith(os.path.join(BASE_DIR, 'static', 'gen')):
+                    print(f"Skipping '{file}' in 'static/gen' folder.")
+                    continue
                 with open(file_path) as file:
                     lines = file.readlines()
                     for line in lines:
+                        if "// LIBRARY FILE" in line:
+                            print(f"Skipping '{file_path}' due to library file!")
+                            break
                         if "console.log(" in line:
-                            print(len(line))
                             line = line.replace("console.log(", "// console.log(")
-                            # print(line)
-        # break
+                        if "console.debug(" in line:
+                            line = line.replace("console.log(", "// console.log(")
+                        file.writelines(lines)
 
 
 if __name__ == "__main__":
