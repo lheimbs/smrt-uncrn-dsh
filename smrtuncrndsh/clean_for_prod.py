8f80b5b177450d15bf31a84b0a1518601b68f913
Lennart Heimbs <l.heimbs@gmail.com>
2020-12-20 12:52:14 +0100

    add skit for this file

--- smrtuncrndsh/clean_for_prod.py
+++ smrtuncrndsh/clean_for_prod.py
@@ -3,7 +3,6 @@ import os
 
 def clean_for_prod():
     BASE_DIR = os.path.abspath(os.path.abspath(os.path.dirname(__file__)))
-    print(BASE_DIR)
     for root, dirs, files in os.walk(BASE_DIR):
         for file in files:
             file_path = os.path.join(root, file)
@@ -12,17 +11,24 @@ def clean_for_prod():
                 if file_path.startswith(os.path.join(BASE_DIR, 'static', 'gen')):
                     print(f"Skipping '{file}' in 'static/gen' folder.")
                     continue
+                if file == __file__.split(os.sep)[-1]:
+                    print("Skipping this file")
+                    continue
                 with open(file_path) as file:
                     lines = file.readlines()
                     for line in lines:
+                        change = False
                         if "// LIBRARY FILE" in line:
                             print(f"Skipping '{file_path}' due to library file!")
                             break
                         if "console.log(" in line:
+                            change = True
                             line = line.replace("console.log(", "// console.log(")
                         if "console.debug(" in line:
+                            change = True
                             line = line.replace("console.log(", "// console.log(")
-                        file.writelines(lines)
+                        if change:
+                            file.writelines(lines)
 
 
 if __name__ == "__main__":
