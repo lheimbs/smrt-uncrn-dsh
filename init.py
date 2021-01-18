99f82d9f6d679c0ab69e65b7a35ea37e94640ce8
Lennart Heimbs <l.heimbs@gmail.com>
2021-01-18 14:26:47 +0100

    Update requirements and init.py

--- init.py
+++ init.py
@@ -1,25 +1,14 @@
 import logging
 
-from dashboard.app import app, server
-# from .index import layout
-from dashboard.misc.generate_shopping_data import generate_shopping_data
+from smrtuncrndsh import create_app
 
 logger = logging.getLogger()
 
-# app.layout = layout
-
 if __name__ == "__main__":
-    logger.debug(f"Environment: {server.config['ENV']}")
+    app = create_app()
+    logger.debug(f"Environment: {app.config['ENV']}")
 
-    if server.config['DEBUG']:
-        logger.debug(f"Debug: {server.config['DEBUG']}")
-        logger.debug(f"Secret key: {server.config['SECRET_KEY']}")
-        logger.debug(f"DATABASE_URI: {server.config['SQLALCHEMY_DATABASE_URI']}")
-
-        logger.debug("Generating development shopping lists.")
-        generate_shopping_data()
-
-    app.run_server(
+    app.run(
         debug=True,
         port=5000,
         host='0.0.0.0',
